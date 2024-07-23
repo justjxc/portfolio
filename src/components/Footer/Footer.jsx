ef0a0a3aeedc42d27a09557dd5ccd6cd8d0f94c0
justjxc <cnhfyybr28@mail.ru>
2024-07-23 17:34:22 +0800

    Updates

--- src/components/Footer/Footer.jsx
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-	return <footer className='w-screen h-screen bg-stone-900'></footer>;
-};
-
-export default Footer;
--- src/components/Layout/Layout.jsx
+++
@@ -1,5 +0,0 @@
-const Layout = ({ children }) => {
-	return <div className='max-w-[1440px] h-full mx-auto my-0'>{children}</div>;
-};
-
-export default Layout;
