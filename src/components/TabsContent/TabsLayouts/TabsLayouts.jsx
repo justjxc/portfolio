facb27b68d925dd6e3be4e3bb0a051385d67133c
justjxc <cnhfyybr28@mail.ru>
2024-07-25 18:08:38 +0800

    added links to lyaouts

--- src/components/TabsContent/TabsLayouts/TabsLayouts.jsx
+++ src/components/TabsContent/TabsLayouts/TabsLayouts.jsx
@@ -8,11 +8,11 @@ import TabsItem from '../TabsItem/TabsItem';
 const TabsLayouts = () => {
 	return (
 		<ul className='grid grid-cols-3 w-full h-full gap-12'>
-			<TabsItem src={layoutImage1} href='#' />
-			<TabsItem src={layoutImage2} href='#' />
-			<TabsItem src={layoutImage3} href='#' />
-			<TabsItem src={layoutImage4} href='#' />
-			<TabsItem src={layoutImage5} href='#' />
+			<TabsItem src={layoutImage1} href='https://justjxc.github.io/layout-thrive/' />
+			<TabsItem src={layoutImage2} href='https://justjxc.github.io/layout-taster/' />
+			<TabsItem src={layoutImage3} href='https://justjxc.github.io/lauout-building/' />
+			<TabsItem src={layoutImage4} href='https://justjxc.github.io/layout-koin/' />
+			<TabsItem src={layoutImage5} href='https://justjxc.github.io/layout-tasteEat/' />
 		</ul>
 	);
 };
